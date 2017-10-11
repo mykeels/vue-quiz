0954a3c9f0c2c973443a52374b2d7bb7d51d5a12
Ikechi Michael <mykehell123@gmail.com>
2017-10-11 15:19:09 +0100

    chore: added ResultItem and installed vuex

--- src/App.vue
+++ src/App.vue
@@ -7,23 +7,9 @@
         <li class=""></li>
       </ol>
       <div class="carousel-inner" role="listbox">
-        <StartItem :is-active="false" />
-        <QuestionItem :is-active="true" />
-        <div class="item" height="600px">
-          <h1 class="quiz-title">Wow you got a perfect score!</h1>
-          <div class="results-ratio">You got 100% of the questions correct!</div>
-          <div class="quiz-answers">
-            <button class="quiz-button btn">Try again?</button>
-          </div>
-          <div class="results-social">
-            <a href="http://twitter.com/intent/tweet?text=I%20got%203%20out%20of%203%20on%20%40urbaninstitute's%20%22How%20well%20do%20you%20know%20real%20creatures%3F%22%20quiz.%20Test%20your%20knowledge%20here%3A%20http%3A%2F%2Furbaninstitute.github.io%2Fquick-quiz%2F">
-              <span class="social social-twitter follow-tw"></span>
-            </a>
-            <a href="https://www.facebook.com/sharer/sharer.php?u=http://urbaninstitute.github.io/quick-quiz/">
-              <span class="social social-facebook follow-fb"></span>
-            </a>
-          </div>
-        </div>
+        <StartItem :is-active="state === STATES.GREETINGS" />
+        <QuestionItem :is-active="state === STATES.QUESTIONS" :question="quiz" />
+        <ResultItem :is-active="state === STATES.RESULTS" />
       </div>
     </div>
   </div>
@@ -33,44 +19,56 @@
 import './App.css'
 import StartItem from './components/StartItem'
 import QuestionItem from './components/QuestionItem'
+import ResultItem from './components/ResultItem'
 
 export default {
   name: 'app',
   components: {
     StartItem,
-    QuestionItem
+    QuestionItem,
+    ResultItem
   },
   data() {
     return {
-      questions: [{
-        title: "One of the following is not like the others",
-        children: [
-          {
-            title: "Loch Ness Monster"
-          },
-          {
-            title: "Centaur"
-          },
-          {
-            title: "Dinosaur"
-          },
-          {
-            title: "Mermaid"
-          }
-        ]
+      STATES: {
+        GREETINGS: 0,
+        QUESTIONS: 1,
+        RESULTS: 2
       },
-      {
+      state: 0,
+      quiz: {
         title: "Do you brush your teeth everyday?",
         children: [
           {
-            title: "Yes"
+            title: "Yes",
+            children: {
+              title: "Do you use toothpaste?",
+              children: [
+                {
+                  title: "Yes"
+                },
+                {
+                  title: "No"
+                }
+              ]
+            }
           },
           {
-            title: "No"
+            title: "No",
+            children: {
+              title: "Do you know about plaque?",
+              children: [
+                {
+                  title: "Yes"
+                },
+                {
+                  title: "No"
+                }
+              ]
+            }
           }
         ]
       }
-      ]
     }
   }
 }
---
+++ src/components/ResultItem.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="item" :class="{ active: isActive }" height="600px">
+    <h1 class="quiz-title">Wow you got a perfect score!</h1>
+    <div class="results-ratio">You got 100% of the questions correct!</div>
+    <div class="quiz-answers">
+      <button class="quiz-button btn">Try again?</button>
+    </div>
+    <div class="results-social">
+      <a href="http://twitter.com/intent/tweet?text=I%20got%203%20out%20of%203%20on%20%40urbaninstitute's%20%22How%20well%20do%20you%20know%20real%20creatures%3F%22%20quiz.%20Test%20your%20knowledge%20here%3A%20http%3A%2F%2Furbaninstitute.github.io%2Fquick-quiz%2F">
+        <span class="social social-twitter follow-tw"></span>
+      </a>
+      <a href="https://www.facebook.com/sharer/sharer.php?u=http://urbaninstitute.github.io/quick-quiz/">
+        <span class="social social-facebook follow-fb"></span>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['isActive']
+}
+</script>
+
+<style>
+
+</style>
